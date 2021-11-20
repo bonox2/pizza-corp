0fa64b0f59f7f84b535090977976784449f60e90
bonox2 <illiagoncharov99@gmail.com>
2021-11-19 21:10:41 -0500

    adaptation 320px index html ubdate

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
-;const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector('.menu-btn');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add {'open'};
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+});
+
+
+document.body.addEventListener('click',  (event) => {
+    const burgerBtn = event.target.closest('.menu-btn')
+    if (burgerBtn) {
+        document.body.classList.toggle('show-menu')
     }
 })
